3178a27892aa88aa52d3160f688792bcd8384d7d
Adran Farias Carnavale <adran.carnavale@gmail.com>
2023-02-22 19:25:55 -0300

    feat: adds function to handle new possibilities

--- internal/GPTClient/RetrieveAIAPIKey.go
+++ internal/GPTClient/RetrieveAIAPIKey.go
@@ -1,10 +1,35 @@
 package GPTClient
 
 import (
+	"fmt"
 	"os"
 )
 
-func RetriveAIAPIKey() string {
-	key := os.Getenv("API_KEY")
-	return key
+var fileName = ".explainrc"
+
+func RetrieveAIAPIKey() (key string, retrieveAIAPIKeyError error) {
+	fmt.Println("Retrieving API key from environment variable...")
+	key = os.Getenv("API_KEY")
+	if key == "" {
+		fmt.Println("Environment variable API_KEY not found.")
+	}
+	keyPrefix := key[0:2]
+	if keyPrefix == "sk" {
+		return key, nil
+	}
+	fmt.Println("Could not retrieve API key from environment variable.")
+
+	fmt.Println("Retrieving API key from file...")
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	filePath := fmt.Sprintf("%s/%s", homeDir, fileName)
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	key = string(file)
+
+	return key, nil
 }
