91b3e7d0b926bcdf2951f09cc53e4490ded70686
Adran Farias Carnavale <adran.carnavale@gmail.com>
2023-02-22 10:46:35 -0300

    refact: modifies function that retrieves API key

--- internal/GPTClient/RetrieveAIAPIKey.go
+++ internal/GPTClient/RetrieveAIAPIKey.go
@@ -1,22 +1,12 @@
 package GPTClient
 
 import (
-	"fmt"
 	"os"
 )
 
-func RetriveAIAPIKey() (string, error) {
-	keyFromEnv := os.Getenv("API_KEY")
-	if keyFromEnv != "" {
-		return keyFromEnv, nil
-	}
-	dir, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
-	key, err := os.ReadFile(fmt.Sprintf("%s/.explainrc", dir))
-	if err != nil {
-		return "", err
-	}
-	return string(key), nil
+var AI_API_KEY_LENGTH = 52
+
+func RetriveAIAPIKey() string {
+	key := os.Getenv("API_KEY")
+	return key
 }
