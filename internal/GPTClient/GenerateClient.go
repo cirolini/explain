fed0fd79003722656b2fb3291feb4b2249c75be9
Adran Farias Carnavale <adran.carnavale@gmail.com>
2023-02-23 00:36:15 -0300

    refact: removes unexpected console print

--- internal/GPTClient/GenerateClient.go
+++ internal/GPTClient/GenerateClient.go
@@ -3,14 +3,12 @@ package GPTClient
 
 import (
 	"context"
-	"fmt"
 
 	gogpt "github.com/sashabaranov/go-gpt3"
 )
 
 // GenerateClient generates a GPT-3 client
 func GenerateClient(key string) (*gogpt.Client, context.Context) {
-	fmt.Println(key)
 	GPTClient := gogpt.NewClient(key)
 	GPTContext := context.Background()
 
